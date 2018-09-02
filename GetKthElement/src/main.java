141e4fe1740e3f460461dd925c59743a3803a36c
JohnsonLu3 <JohnsonLu94@gmail.com>
2018-09-01 23:36:01 -0400

    added find intersection

--- GetKthElement/src/main.java
+++ GetKthElement/src/main.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class main {
 
     /**
@@ -27,18 +29,48 @@ public class main {
         return n;
     }
 
+    public static Node printIntersection(Node n, Node b){
+        if(n == null || b == null) return null;
+
+        HashSet<Node> nodes = new HashSet<>();
+
+        while(n != null){
+                nodes.add(n);
+                n = n.next;
+        }
+
+        while(b != null){
+            if(nodes.contains(b))
+                return b;
+
+            b = b.next;
+        }
+
+        return null;
+    }
 
     public static void main(String args[]){
 
         Node n1 = new Node(2);
         Node n2 = new Node(6);
-        Node n3 = new Node(1);
+        Node n3 = new Node(7);
         Node n4 = new Node(3);
+
+        Node b1 = new Node(2);
+        Node b2 = new Node(2);
+        Node b3 = new Node(3);
+        Node b4 = new Node(4);
 
         n1.next = n2;
         n2.next = n3;
         n3.next = n4;
 
+        b1.next = b2;
+        b2.next = b3;
+        b3.next = b4;
+        b4.next = n3;
+
         System.out.println(returnKthelement(n1, 2).val);
+        System.out.println(printIntersection(n1, b1).val);
     }
 }
